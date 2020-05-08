7ab695c4be78e5d15be50707e30c3c837d1a3608
Vijay PD <vijaypd2@gmail.com>
2020-05-08 12:00:45 +0530

    Working before ui update

---
+++ app.js
@@ -0,0 +1,48 @@
+//jshint esversion:8
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://localhost:27017/wallDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const postSchema = new mongoose.Schema({
+  title: String,
+  desc: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+app.listen(3000, function() {
+  console.log("Server Started at port 3000");
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post('/', async(req, res) => {
+    const post = new Post({
+        title: req.body.postTitle,
+        desc: req.body.postDesc
+    });
+
+    try {
+        await post.save();
+        const posts = await Post.find();
+        res.send(posts);
+    } catch (err) {
+        res.send(err);
+    }
+
+    res.end();
+});
